70599364a84cfed040dc55b82af6e8679bd556f6
cricheson <christian.richeson@gmail.com>
2016-03-05 15:27:05 -0600

    more print statements added

--- character-sheet/test.py
+++ character-sheet/test.py
@@ -1,17 +1,19 @@
 from stats import *
 from skills import *
+from ac import *
 
-steve = Stats(15,16,12,13,14,16)
+steve = Stats(21,16,12,13,14,16,9)
 steve_skills = Skills(steve)
+steve_ac = WhatsMyAC(steve)
 
 
-#print steve.getstr()
 
-#skill_dict = {
-#		'athletics': steve.getstrmod()
-#	}
 
-print steve.str.val
-print steve_skills.skill_dict['athletics']()
-print steve_skills.is_proficient('perception')
-print steve_skills.passive_perception_calc("no")
+#print steve.str.val
+#print steve.str.modifier()
+#print steve_skills.skill_dict['athletics']()
+#print steve_skills.is_proficient('perception')
+#print steve_skills.passive_perception_calc("no")
+#print steve.lvl.val
+#print steve.lvl.prof_bonus()
+print steve_ac.ac_calc()
